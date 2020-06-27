b736d8f3cc4e7ebdcfa6a66e1e934575746772fa
sidyadav2211 <kailashyadav1998@example.com>
2020-06-27 19:35:58 +0530

    Updates

---
+++ precache-manifest.25087efc84aef5b9ca7d2789d7062550.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7ad8efe07667d12998290b9ab4a78d",
+    "url": "/myapp-Filter-Reading/index.html"
+  },
+  {
+    "revision": "368c58cfd6de1ccaf523",
+    "url": "/myapp-Filter-Reading/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b012d8d6e6bc90dd1faf",
+    "url": "/myapp-Filter-Reading/static/css/main.e1305a31.chunk.css"
+  },
+  {
+    "revision": "368c58cfd6de1ccaf523",
+    "url": "/myapp-Filter-Reading/static/js/2.da5aee02.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/myapp-Filter-Reading/static/js/2.da5aee02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b012d8d6e6bc90dd1faf",
+    "url": "/myapp-Filter-Reading/static/js/main.4eb00407.chunk.js"
+  },
+  {
+    "revision": "172ef8c12997ce386122",
+    "url": "/myapp-Filter-Reading/static/js/runtime-main.1143bc79.js"
+  }
+]);
